bb660d23744dc17265e56e54b4893a9941ed8b99
yangshaoning <yangshaoning@didiglobal.com>
2021-08-26 16:23:56 +0800

    chore: 为部分函数添加说明

--- app/src/main/java/com/annotation/AnnotationHelper.kt
+++ app/src/main/java/com/annotation/AnnotationHelper.kt
@@ -5,7 +5,9 @@ object AnnotationHelper {
         val clazz = any::class.java
         val fields = clazz.declaredFields
         fields.forEach {
+            // 检查当前元素是否被指定注解修饰
             if (it.isAnnotationPresent(PersonAnnotation::class.java)) {
+                // 查找指定注解
                 val personAnnotation = it.getAnnotation(PersonAnnotation::class.java)
                 it.isAccessible = true
                 val obj = it.type.getConstructor().newInstance()
