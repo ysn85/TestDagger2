564c2e0144de8a1b6d847e3a25d36f315560d069
yangshaoning <yangshaoning@didiglobal.com>
2021-09-07 11:51:53 +0800

    feat: 新增网络请求Demo实现

--- app/src/main/java/com/live/demo/TestDataSource.kt
+++ app/src/main/java/com/live/demo/TestDataSource.kt
@@ -1,10 +1,17 @@
 package com.live.demo
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.live.demo.TestViewModel.Companion.TAG
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.URL
 
 class TestDataSource : DataSource {
     private val mNameData = MutableLiveData<String>()
@@ -16,6 +23,37 @@ class TestDataSource : DataSource {
         withContext(Dispatchers.Main) {
             mNameData.value = "Loading..."
             mNameData.value = fetchRealData()
+            mNameData.value = fetchDataFromNet()
+        }
+    }
+
+    private suspend fun fetchDataFromNet(): String = withContext(Dispatchers.IO) {
+        val url = URL("https://www.baidu.com/")
+        var inputStream: InputStream? = null
+        var bufferedReader: BufferedReader? = null
+        val stringBuilder = StringBuilder()
+        try {
+            val httpURLConnection = url.openConnection()
+            httpURLConnection.connect()
+            inputStream = httpURLConnection.getInputStream()
+            bufferedReader = BufferedReader(InputStreamReader(inputStream))
+            var readLineStr: String? = bufferedReader.readLine()
+            while (readLineStr != null) {
+                stringBuilder.append(readLineStr).append("\n")
+                readLineStr = bufferedReader.readLine()
+            }
+            Log.i(TAG, "fetchDataFromNet$stringBuilder")
+        } catch (ioE: IOException) {
+            ioE.printStackTrace()
+        } finally {
+            bufferedReader?.close()
+            inputStream?.close()
+        }
+//        delay(2000)
+        if (stringBuilder.isEmpty()) {
+            "Hello empty Data"
+        } else {
+            stringBuilder?.substring(0, 10)
         }
     }
 
