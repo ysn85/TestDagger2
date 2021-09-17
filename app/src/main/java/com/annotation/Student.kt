483857b4e7f8b623cdfb53e098ed9c3a48ff2fd5
yangshaoning <yangshaoning@didiglobal.com>
2021-09-17 20:26:33 +0800

    chore: 调整注解配置

--- app/src/main/java/com/annotation/Student.kt
+++ app/src/main/java/com/annotation/Student.kt
@@ -1,23 +1,13 @@
 package com.annotation
 
+@TestAnnotation
 class Student {
+    @TestAnnotation
     var name: String? = null
     var age: Int = 0
 
-    @Deprecated(
-        message = "age illegal",
-        replaceWith = ReplaceWith("${"%d"}", "00"),
-        level = DeprecationLevel.WARNING
-    )
     @TestAnnotation
-    fun getSuper(): Int {
-        return age
-    }
-
-    override fun toString(): String {
-        return "StudentJ{" +
-                "age=" + age +
-                "name=" + name +
-                "}"
+    fun getStudentInfo(): String {
+        return "name is $name age is $age"
     }
 }
--- app/src/main/java/com/annotation/TestAnnotation.kt
+++ app/src/main/java/com/annotation/TestAnnotation.kt
@@ -2,6 +2,6 @@ package com.annotation
 
 @MustBeDocumented
 @Retention(value = AnnotationRetention.BINARY)
-@Target(AnnotationTarget.FIELD, AnnotationTarget.FUNCTION)
+@Target(AnnotationTarget.FIELD, AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 annotation class TestAnnotation() {
 }
