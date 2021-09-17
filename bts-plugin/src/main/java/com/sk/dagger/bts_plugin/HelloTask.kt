4ce31ec21b8d4698155baeff95a0f81fba75ca2f
yangshaoning <yangshaoning@didiglobal.com>
2021-09-17 12:12:55 +0800

    chore: 添加task类注释

--- bts-plugin/src/main/java/com/sk/dagger/bts_plugin/HelloTask.kt
+++ bts-plugin/src/main/java/com/sk/dagger/bts_plugin/HelloTask.kt
@@ -3,6 +3,9 @@ package com.sk.dagger.bts_plugin
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
+/**
+ * Task一定要加open修饰符否则，不能默认，默认为final修饰符
+ */
 open class HelloTask : DefaultTask() {
 
     init {
